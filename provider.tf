62b10f007749e9ff829cb9c9d41853c70d664ee6
Cristian Beraha <cristian@cloudops.works>
2022-03-27 23:49:31 -0300

    update

--- provider.tf
+++ provider.tf
@@ -1,5 +1,11 @@
 provider "aws" {
   profile = var.aws_profile
   region  = var.aws_region
+
+  assume_role {
+    role_arn     = var.aws_sts_arn
+    session_name = "terraform-session"
+    external_id  = "terraform"
+  }
 }
 
